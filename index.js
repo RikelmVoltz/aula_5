6259d2bc6a70ec638a3bf9b69dbff6692bfd30ab
RikelmVoltz <142436269+RikelmVoltz@users.noreply.github.com>
2023-08-21 17:31:14 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+/*
+* primeiro programa em javascript 
+* rikelme voltz
+*/
+
+console.log("ola mundo")
+// prompt("qual é o seu nome?")
+
+
+const variavelA = 100;
+console.log (variavelA);
+const variavelB = variavelA + 50;
+console.log(variavelA)
+console.log(variavelB)
+const variavelC = variavelA / variavelB;
+console.log(variavelC);
+
+
+let numero = 5;
+console.log(numero);
+numero = 120 + 5;
+console.log(numero)
+//Concatenar textos
+/*
+let nome = "rikelme";
+let sobreNome = "Voltz";
+let idadade = 16;
+console.log ("ola", nome, sobreNome, "voce tem", idade, 'anos');
+*/
+const variavelFalsa = false;
+const variavelVerdadeira = true;
+
+
+// Exercicio 1 
+
+const nome = "rikelme";
+const sobreNome = "voltz";
+const idade = 15;
+const status = "você é estudante";
+console.log("Ola",nome,sobreNome,idade,status);
+ 
+//typeof
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof estudande);
+
+
+
+const vazio = null;
+console.log(typeof vazio);
+
+
+//prompt 
+
+let valor = prompt("seu nome");
+console.log(valor);
+
+//exercicio 2 
+
+let exercicio = prompt("digite seu nome");
+let atividade = prompt("digite sua idade");
+console.log(typeof exercicio);
+console.log(typeof atividade);
